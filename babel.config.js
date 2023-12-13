7ef6f141dbd4c6d2c05cbd78682dc24474c9eaa9
Dante Benitez <benitezdante123@gmail.com>
2023-12-13 00:37:35 -0300

    feat: Counter component

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,22 @@
-module.exports = function(api) {
+const path = require("path");
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-    [
-       'module-resolver',
-       {
-         root: ['./src'],
-         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-         alias: {
-          "@": ["./"]
-         }
-       }
-    ]
-  ]
+      ['module-resolver',
+      {
+        root: ["./src"],
+        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+        alias: {
+          "^@/(.*)": ([, pathname]) => { 
+            const resolved = path.resolve("src", pathname);
+            console.log('resolved', resolved);
+            return resolved;
+          }
+        },
+      },
+    ]],
   };
 };
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,12 +7,13 @@ export function Counter({ initialCount = 0 }) {
     const increment = () => setCount(c => c + 1);
     const decrement = () => setCount(c => c - 1);
     const reset = () => setCount(initialCount);
+
     return (
-        <View>
+        <View style={styles.container}>
             <View style={styles["btn-container"]}>
-                <Button title="Increment" onPress={increment} color="blue"/>
+                <Button title="Increment" onPress={increment} color="blue" />
+                <Button title="Reset" onPress={reset} color="#000" />
                 <Button title="Decrement" onPress={decrement} color="red" />
-                <Button title="Reset" onPress={reset} color="yellow" />
             </View>
             <Text>Count is {count}</Text>
         </View>
@@ -20,8 +21,17 @@ export function Counter({ initialCount = 0 }) {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 20
+    },
     'btn-container': {
+        width: '50%',
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
+        gap: 4,
     }
 })
